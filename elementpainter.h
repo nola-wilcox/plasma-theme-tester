fc0e5b2c7f10a7862eea5a57fc5ef004754d39f0
Nola Wilcox <nola.wilcox@inbox.ru>
2021-04-10 13:05:04 +0300

    Initial commit

---
+++ elementpainter.h
@@ -0,0 +1,50 @@
+/*
+  Copyright (c) 2008 Riccardo Iaconelli <riccardo@kde.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
+#ifndef ELEMENTPAINTER_H
+#define ELEMENTPAINTER_H
+
+#include <QWidget>
+
+class QPaintEvent;
+class QString;
+
+namespace Plasma{
+    class Svg;
+    class SvgPanel;
+};
+
+class ElementPainter : public QWidget
+{
+public:
+    ElementPainter(QObject *o);
+    ~ElementPainter();
+    void setType(const QString &t);
+    QString type();
+    void refresh();
+
+// public Q_SLOTS:
+//     void launchSvgSelector();
+
+protected:
+    virtual void paintEvent(QPaintEvent *p);
+    virtual void mouseReleaseEvent(QMouseEvent *e);
+    virtual void resizeEvent(QResizeEvent *e);
+
+private:
+    Plasma::Svg *m_renderer;
+    Plasma::SvgPanel *m_panelRenderer;
+    QString m_type;
+    QString m_svgPath;
+    bool m_usePanelSvg;
+};
+
+
+#endif
+
